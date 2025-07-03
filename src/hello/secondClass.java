866dd2f98014c02206f3fb99a35a7ad1df1f29df
GhostVihan <princevihan044@gmail.com>
2025-07-03 20:34:38 +0530

    3rd commit

---
+++ src/hello/secondClass.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class secondClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			 String a = "3rd class";
+			 System.out.println(a);
+	}
+
+}
